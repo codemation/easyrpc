f20c5a525167210c5803e14506b5445459fa7bef
codemation <joshjamison1@gmail.com>
2020-12-28 22:54:32 +0100

    updated with pyjwt 2.0 version api - which do ot require manually decoding / encoding tokens

--- easyrpc/auth.py
+++ easyrpc/auth.py
@@ -2,14 +2,14 @@ import os, uuid, time, json, base64, jwt, string, random
 
 def encode(secret, log=None, **kw):
     try:
-        return jwt.encode(kw, secret, algorithm='HS256').decode()
+        return jwt.encode(kw, secret, algorithm='HS256')
     except Exception as e:
         if log:
             log.exception(f"error encoding {kw} using {secret}")
 
 def decode(token, secret, log=None):
     try:
-        return jwt.decode(token.encode('utf-8'), secret, algorithm='HS256')
+        return jwt.decode(token, secret, algorithms='HS256')
     except Exception as e:
         if log:
             log.exception(f"error decoding token {token} using {secret}")
